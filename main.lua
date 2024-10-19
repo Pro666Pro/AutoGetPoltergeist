b1a0cc178e7bb615aad87045eb0eecbeb818a85c
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-20 01:46:31 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,19 +4,35 @@ local teleportFunc = queueonteleport or queue_on_teleport
             if not game:IsLoaded() then
                 game.Loaded:Wait()
             end
+
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Loading",Text = "Script Loading :-0",Icon = "rbxassetid://7733658504",Duration = 
 
-aura = true
-		
-if aura == true do
+local GameName = "AutoGetPoltergeist - by nexer"
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({IntroText = "nexer script >:]", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4370318685",
+	PremiumOnly = false
+})
+
+Tab:AddToggle({
+	Name = "Auto Get Poltergeist",
+	Default = true,
+	Callback = function(Value)
+		_G.AutoSlapAll = Value
+while _G.AutoSlapAll do
 for i,v in pairs(workspace.Enemies:GetChildren()) do
 if v:FindFirstChild("HumanoidRootPart") then
 game:GetService("ReplicatedStorage").Remotes.GeneralHit:FireServer(v:FindFirstChild("HumanoidRootPart"))
 end
 end
 task.wait(0.15)
-until aura == false
 end
+	end    
+})
 		
 ]])  
 end

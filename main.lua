abf46a01244c41b8e2813597f796190f9abd1aa4
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-20 11:16:38 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,33 +5,27 @@ local teleportFunc = queueonteleport or queue_on_teleport
                 game.Loaded:Wait()
 	end
 
-local GameName = "AutoGetPoltergeist - by nexer"
-
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "nexer script >:]", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
-
-local Tab = Window:MakeTab({
-	Name = "Main",
-	Icon = "rbxassetid://4370318685",
-	PremiumOnly = false
-})
-
-Tab:AddToggle({
-	Name = "Auto Get Poltergeist",
-	Default = true,
-	Callback = function(Value)
-		_G.AutoSlapAll = Value
-while _G.AutoSlapAll do
 for i,v in pairs(workspace.Enemies:GetChildren()) do
 if v:FindFirstChild("HumanoidRootPart") then
-game:GetService("ReplicatedStorage").Remotes.GeneralHit:FireServer(v:FindFirstChild("HumanoidRootPart"))
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Found",Text = "Server Found" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/PG/main/main.lua'))()
+else
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Not Found",Text = "Not Found" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+wait(0.5)
+serverhop = true
+wait(0.5)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+wait(3)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+wait(3)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+wait(3)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+wait(3)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/serverhop/main/main.lua'))()
+wait(3)
 end
 end
-task.wait(0.15)
-end
-	end    
-})
-		
 ]])  
 end
 game:GetService("TeleportService"):Teleport(103505724406848)
